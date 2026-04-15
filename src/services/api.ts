30ddb310b6c1e79905a5588198c93b9a3a529fb9
gitLunga <lungamngomezulu10@gmail.com>
2026-04-15 17:37:01 +0200

    fix: supabase to local storage

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,9 +11,9 @@ const getBaseURL = () => {
         return 'http://localhost:5000/api';
     } else {
         // return 'http://192.168.137.1:5000/api';
-    //   return 'http://192.168.18.160:5000/api';
+      //return 'http://192.168.18.160:5000/api';
       //   return DEV_BACKEND_URL;
-         return API_URL;
+        return API_URL;
     }
 };
 
